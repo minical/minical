2ef6233f31a95f14c9b706c8f382b42571cf72d5
LAPTOP-BM27UMG1\mradu <mradul.jain90@gmail.com>
2021-06-14 16:35:36 +0530

    implement display-error popup

--- public/application/views/includes/bootstrapped_footer.php
+++ public/application/views/includes/bootstrapped_footer.php
@@ -1,3 +1,19 @@
+<div class="modal fade" id="display-errors">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
+</div>
 
 <input type="hidden" id="companyDateFormat" value="<?php echo isset($this->company_date_format) ? $this->company_date_format : ''; ?>">
 
