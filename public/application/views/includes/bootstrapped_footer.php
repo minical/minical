7a7a36c8b548b585b9abbe86e96caa2f3c544f31
LAPTOP-BM27UMG1\mradu <mradul.jain90@gmail.com>
2021-07-05 18:31:04 +0530

    add display cc details modal

--- public/application/views/includes/bootstrapped_footer.php
+++ public/application/views/includes/bootstrapped_footer.php
@@ -15,6 +15,25 @@
     </div>
 </div>
 
+<div class="modal fade" id="display-cc-details" data-backdrop="static" 
+   data-keyboard="false" style="z-index: 9999;"
+   >
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
+</div>
+
 <input type="hidden" id="companyDateFormat" value="<?php echo isset($this->company_date_format) ? $this->company_date_format : ''; ?>">
 
 <?php
