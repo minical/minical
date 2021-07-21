a1b68f0349b0408e7ee9454445d9ff777cd236ff
Adimahodaya <mahodaya10@gmail.com>
2021-07-21 14:31:42 +0530

    imported the extra's csv

--- public/application/models/extra_model.php
+++ public/application/models/extra_model.php
@@ -200,6 +200,31 @@ class Extra_model extends CI_Model {
 		}
 		return NULL;
 	}
+
+    function create_all_extras($data)
+    {
+
+        $this->db->insert('extra', $data);
+
+        if ($this->db->_error_message())
+        {
+            show_error($this->db->_error_message());
+        }
+        else
+        {
+            //return $this->db->insert_id();
+            $query = $this->db->query('select LAST_INSERT_ID( ) AS last_id');
+            $result = $query->result_array();
+            if(isset($result[0]))
+            {
+                return $result[0]['last_id'];
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
 	
 }
 
