0d7cdfe69a2321db278b5e6a9311224211dc9bda
Adimahodaya <mahodaya10@gmail.com>
2021-08-18 16:45:34 +0530

    statement added in import

--- public/application/views/hotel_settings/company/import.php
+++ public/application/views/hotel_settings/company/import.php
@@ -17,7 +17,8 @@
 <div class="main-card card">
     <div class="card-body">
 
-        <form enctype="multipart/form-data" method="post" action="/settings/company/import_company_data">
+        <form enctype="multipart/form-data" method="post" action="/settings/company/import_company_data"  onsubmit="$('#loading').show();">
+
             <h4><?php echo l('Import zip file:'); ?> </h4>
 
             Download zip template <a target="_blank" href="/upload/import-template.zip">here.</a>
@@ -27,6 +28,8 @@
             <input type="checkbox" id="old_data" name="removd_old_data" value="1">
             <label for="old_data"><?php echo l('Remove old data (Recommended)'); ?></label><br>
             <br>
+            <div id="loading" style="display:none"><img src= "<?php echo base_url().'images/loading.gif'; ?>"></div>
+            <br/>
             <input type="submit" name="submit" value="Start Import" class="btn btn-primary"> <br><br>
 
         </form>
