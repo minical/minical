4ed085b8efa6b1ce35604191aadab714a36cca53
poojasahu95 <pooja.quazma@gmail.com>
2021-11-22 19:16:51 +0530

    post and postmeta functions

--- public/application/helpers/includes/post_helper.php
+++ public/application/helpers/includes/post_helper.php
@@ -28,14 +28,121 @@ function add_post ($data) {
     do_action('post.add.post', $post_id, $data, $CI->input->post());
 }
 
-// function get_post(){
+/**
+ * get post form posts table
+ * 
+ * @param array $post An Array of post data including post_id (require), additional data will be added in where clause.
+ * @return mixed Either array or null, if post data is available or no error occurs, then array else null.
+ * 
+ */
+function get_post(array $post = null)
+{
 
-//     $CI = & get_instance();
+    $post_data = null;
+    $CI = & get_instance();
+    $CI->load->model('Post_model');
 
-//     $CI->load->model('Post_model');
+    // fillters 
+    $post = apply_filters( 'before_get_post', $post, $CI->input->post());
+    $should_get_post = apply_filters( 'should_get_post', $post, $CI->input->post());
+    if (!$should_get_post) {
+        return;
+    }
 
-//     $data = $CI->Post_model->get_post();
+    if(empty($post)){
+        return null;
+    }else{
 
-//     return $data;
+        //for action befor getting the post
+        do_action('pre.get.post', $post, $CI->input->post()); 
+        $post_data = $CI->Post_model->get_post($post);
+    }
 
-// }
+    if(!$post_data){
+        return null;
+    }
+
+    $post_id = $post_data['post_id'];
+    //for action after getting the post
+    do_action('post.get.post', $post_id, $post, $CI->input->post());
+    return $post_data;
+    
+}
+
+/**
+ * edit a post data of posts table
+ * 
+ * @param array $post An Array of post data including post_id.
+ * @return mixed Either post_id or null, if post data is updated or no error occurs, then id else null.
+ */
+function edit_post(array $post = null)
+{
+    $CI = & get_instance();
+    $CI->load->model('Post_model');
+
+    // fillters 
+    $post = apply_filters( 'before_edit_post', $post, $CI->input->post());
+    $should_edit_post = apply_filters( 'should_edit_post', $post, $CI->input->post());
+    if (!$should_edit_post) {
+        return;
+    }
+
+    if(empty($post)){
+        return null;
+    }
+    
+    //for action befor editing the post
+    do_action('pre.edit.post', $post, $CI->input->post()); 
+
+    $updated_flag = $CI->Post_model->edit_post($post);
+    if(empty($updated_flag)){
+        return null;
+    }
+
+    $post_id = $post_data['post_id'];
+
+    //for action after editing the post
+    do_action('post.edit.post', $post_id, $post, $CI->input->post());
+    
+    
+    return $post_id;
+
+}
+
+/**
+ * delete a post data.
+ * 
+ * @param int $post_id The id of the post.
+ * @return mixed Either true or null, if postmeta data is delete then true else null.
+ * 
+ */
+function delete_post(int $post_id = null)
+{
+    $CI = & get_instance();
+    $CI->load->model('Post_model');
+
+    // fillters 
+    $post = apply_filters( 'before_delete_post', $post_id, $CI->input->post());
+    $should_delete_post = apply_filters( 'should_delete_post', $post_id, $CI->input->post());
+    if (!$should_delete_post) {
+        return;
+    }
+
+    if(isset($post_id) && $post_id == null){
+        return null;
+    }
+
+    //for action befor deleteting the post
+    do_action('pre.delete.post', $post_id, $CI->input->post()); 
+  
+    $delete_flag = $CI->Post_model->delete_post($post_id);
+    if(empty($delete_flag)){
+        return null;
+    }
+    
+    //for action after deleteting the post
+    do_action('post.delete.post', $post_id, $CI->input->post());
+  
+    return true;
+    
+}
--- public/application/models/post_model.php
+++ public/application/models/post_model.php
@@ -13,4 +13,128 @@ class Post_model extends CI_Model {
         return $this->db->insert_id();
     }
 
+    function get_post($post)
+    {
+        $this->db->select('*');
+       
+        if(isset($post) && count($post)>0){
+            foreach($post as $key=>$value){
+             $this->db->where($key, $value);      
+            }
+        }elseif(is_int($post))
+        {
+            $this->db->where('post_id', $post);   
+        }
+
+
+        $query = $this->db->get('posts');
+        if ($this->db->_error_message())
+        {
+            show_error($this->db->_error_message());
+        }
+        //  echo $this->db->last_query();die;
+        $result_array = $query->result_array();
+
+        return $result_array;
+    }
+
+    function edit_post(array $post = null)
+    {
+        $this->db->where('post_id', $post['post_id']);
+        $this->db->update('posts', $post);
+        if ($this->db->_error_message())
+        {
+            show_error($this->db->_error_message());
+        }else{
+            if ($this->db->affected_rows() > 0){
+                return TRUE;
+            }else{
+                return FALSE;
+            }
+        }
+    }
+
+    function delete_post(int $post_id = null)
+    {
+     
+        $data = Array('is_deleted' => '1');
+        $this->db->where('post_id', $post_id);
+        $this->db->update('posts', $data);
+        if ($this->db->_error_message())
+        {
+            show_error($this->db->_error_message());
+        }else{
+            if ($this->db->affected_rows() > 0){
+                $this->db->where('post_id', $post_id);
+                $this->db->delete('postmeta');
+                return TRUE;
+            }else{
+                return FALSE;
+            }
+        }
+    }
+
+    function get_post_meta(int $post_id = null, string $key = null)
+    {
+        
+        $this->db->select('*');
+       
+        if(isset($post_id) && $post_id !== null){
+            $this->db->where('post_id', $post_id);   
+        
+        }
+        if(isset($key) && $key !== null){
+            $this->db->where('meta_key', $key);   
+        
+        }
+        
+        $query = $this->db->get('postmeta');
+        if ($this->db->_error_message())
+        {
+            show_error($this->db->_error_message());
+        }
+       
+        $result_array = $query->result_array();
+
+        return $result_array;
+    }
+
+    function edit_post_meta(array $post_meta = null, int $post_id= null)
+    {
+        $data = array();      
+        foreach ($post_meta as $key => $value) {
+            $data['meta_key']= $key;
+            $data['meta_value'] = $value;
+            $data['post_id'] = $post_id;       
+            $post_meta_data = $this->db->select("*")->where('meta_key', $key)->get('postmeta')->num_rows();
+            if(isset($post_meta_data) && $post_meta_data >0){
+                $this->db->where('post_id', $post_id);
+                $this->db->where('meta_key', $key); 
+                $this->db->update('postmeta', $data);
+            }else {
+                $this->db->insert('postmeta', $data);
+            }
+        }
+        if ($this->db->_error_message())
+        {
+            show_error($this->db->_error_message());
+        }else{
+            if ($this->db->affected_rows() > 0){
+                return TRUE;
+            }else{
+                return FALSE;
+            }
+        }
+    }
+
+    function delete_post_meta(int $post_id = null)
+    {
+        $this->db->where('post_id', $post_id);
+        $this->db->delete('postmeta');
+        if ($this->db->affected_rows() > 0){
+            return TRUE;
+        }else{
+            return FALSE;
+        }   
+    }
 }
