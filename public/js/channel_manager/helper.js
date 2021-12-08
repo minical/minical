3d29b829a1b8e031dcfac74979b4c15e9728985e
Pankaj Patidar <pankaj9296@gmail.com>
2021-12-08 18:11:24 +0530

    Helper updates

--- public/js/channel_manager/helper.js
+++ public/js/channel_manager/helper.js
@@ -3,6 +3,10 @@
 //Function getBaseURL
 //returns baseURL
 var getBaseURL = function () {
-	var url = $('#project_url').val() + '/';
+	var url = $('#project_url').val();
+	url = url ? url : 'app.minical.io';
+	if (url.substring(url.length - 1) !== "/") {
+		url = url + '/';
+	}
 	return url;
 }
--- public/js/helpers.js
+++ public/js/helpers.js
@@ -42,7 +42,11 @@ innGrid.addCommas = function(nStr)
 //Function getBaseURL
 //returns baseURL
 var getBaseURL = function () {
-    var url = $('#project_url').val() + '/';
+    var url = $('#project_url').val();
+    url = url ? url : 'app.minical.io';
+    if (url.substring(url.length - 1) !== "/") {
+        url = url + '/';
+    }
 	return url;
 }
 
