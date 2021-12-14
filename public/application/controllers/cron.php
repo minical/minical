cb68169691c4d254d974bbef3c028b0b0f98f3b1
LAPTOP-BM27UMG1\mradu <mradul.jain90@gmail.com>
2021-12-14 15:41:04 +0530

    Full sync only for configured companies

--- public/application/controllers/cron.php
+++ public/application/controllers/cron.php
@@ -173,7 +173,7 @@ class Cron extends CI_Controller
     function full_sync(){
     	$this->load->model(array('Company_model'));
 
-    	$companies = $this->Company_model->get_all_companies();
+    	$companies = $this->Company_model->get_all_companies(true);
 		
 		foreach ($companies as $company)
 		{
