77e6f8d9178ed4eada57e07417cd16146bf980af
LAPTOP-BM27UMG1\mradu <mradul.jain90@gmail.com>
2022-06-21 12:14:53 +0530

    Show non-deleted partners

--- public/application/models/whitelabel_partner_model.php
+++ public/application/models/whitelabel_partner_model.php
@@ -219,6 +219,7 @@ class Whitelabel_partner_model extends CI_Model {
         $this->db->from('whitelabel_partner AS wp');
         $this->db->join('users AS u', "u.id = wp.admin_user_id", "left");
         $this->db->where('show_on_partners_page', 1);
+        $this->db->where('is_deleted', 0);
         $this->db->group_by('wp.id');
         $query = $this->db->get();
         if($query->num_rows() > 0)
