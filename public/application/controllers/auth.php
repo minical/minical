4f1e915b985e7896a43aea1556c88cad69b40dd3
LAPTOP-BM27UMG1\mradu <mradul.jain90@gmail.com>
2022-08-19 11:45:41 +0530

    get whitelabel by username (case-sensitive)

--- public/application/controllers/auth.php
+++ public/application/controllers/auth.php
@@ -578,7 +578,10 @@ class Auth extends MY_Controller
                 $white_label_detail = $white_label_detail[0];
                 $data['whitelabel_detail'] = $white_label_detail;
             } else {
-                $white_label_detail = $this->Whitelabel_partner_model->get_partners(array('username' => $white_label_name));
+                //$white_label_detail = $this->Whitelabel_partner_model->get_partners(array('username' => $white_label_name));
+
+                $white_label_detail = $this->Whitelabel_partner_model->get_partner_by_username($white_label_name);
+
                 if($white_label_detail)
                 {
                     $white_label_detail = $white_label_detail[0];
