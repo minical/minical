fc2c06215158f5c0b1c1a1fbc42186a77229577e
Harsh Binjwa <94108599+harshbinjwa@users.noreply.github.com>
2023-11-10 13:23:33 +0530

    added total amount and paid amount in the booking tooltip with the balance.

--- public/js/fullcalendar/init.js
+++ public/js/fullcalendar/init.js
@@ -357,7 +357,7 @@ innGrid.buildCalendar = function (rooms) {
         calendar_options['eventMouseEnter'] = function (info) {
             var groupId = (info.event.extendedProps.data.id_group_booking != 'null') ? "<strong>" + l("group_id") + ":</strong> " + info.event.extendedProps.data.id_group_booking : '';
             var groupName = (info.event.extendedProps.data.group_name != 'null') ? "<strong>" + l("group_name") + ":</strong> " + info.event.extendedProps.data.group_name : '';
-            //var totalAmount =  parseFloat(data.balance) + parseFloat(data.payment_total) ;
+           var totalAmount =  parseFloat(info.event.extendedProps.data.balance) + parseFloat(info.event.extendedProps.data.payment_total);
 
             // get booking sources text
             var sources = [];
@@ -399,7 +399,9 @@ innGrid.buildCalendar = function (rooms) {
                 '<p><strong>' + l("Room", true) + ':</strong> ' + info.event.extendedProps.data.room_name + '</p>' +
                 '<p><span><strong>' + l("check_in", true) + ':</strong> ' + innGrid._getLocalFormattedDate(checkInDate) + '</span> <span><strong>' + l("check_out", true) + ':</strong> ' + innGrid._getLocalFormattedDate(checkOutDate) + '</span></p>' +
                 '<p><strong>' + l("Number of nights", true) + ':</strong> ' + diffDays + '</p>' +
-                '<p style="margin-bottom:8px;"> <span><strong>' + l("Amount Due", true) + ':</strong> ' + parseFloat(info.event.extendedProps.data.balance) + '</span></p>' +
+                '<p> <span><strong>' + l("Total Amount", true) + ':</strong> ' + parseFloat(totalAmount) + '</span></p>' +
+                '<p> <span><strong>' + l("Balance Due", true) + ':</strong> ' + parseFloat(info.event.extendedProps.data.balance) + '</span></p>' +
+                '<p style="margin-bottom:8px;"> <span><strong>' + l("Paid Amount", true) + ':</strong> ' + parseFloat(info.event.extendedProps.data.payment_total) + '</span></p>' +
                 '<p><span><strong>' + l("Booking Source", true) + ':</strong> ' + sourceText + '</span></p>' +
                 '<p><div style="max-width: 300px;"><strong>' + l("booking_notes", true) + ':</strong> ' + notes +
                 (warning_message ? ('<p><div style="max-width: 300px;"><strong>' + l("Warning", true) + ':</strong> ' + warning_message) : '') +
